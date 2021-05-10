b8921b24c46eee085a5dd44eb9ba02988074a71e
Matheus Vinicius Cirino e Silva <matheus.vinicius@inatel.br>
2021-05-10 08:42:16 -0300

    Initializing project with start.spring.io

---
+++ src/main/java/br/com/inatel/icc/pastebean/PasteBeanApplication.java
@@ -0,0 +1,13 @@
+package br.com.inatel.icc.pastebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteBeanApplication.class, args);
+	}
+
+}
