b018cd362bd95d7c9eedbb0bd7fee1eb53acf557
Matheus Vinícius Cirino e Silva <matheus.vinicius@inatel.br>
2021-05-12 17:32:33 -0300

    Bugfix on UpdateUserForm + implementing Delete method on UserController

--- src/main/java/br/com/inatel/icc/pastebean/controller/UserController.java
+++ src/main/java/br/com/inatel/icc/pastebean/controller/UserController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,18 @@ public class UserController {
 		if(optional.isPresent()) {
 			User user = userForm.update(id, userRepository);
 			return ResponseEntity.ok(new UserDto(user));
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("{id}")
+	@Transactional
+	public ResponseEntity<?> deleteUsers(@PathVariable Long id){
+		Optional<User> optional = userRepository.findById(id);
+		if(optional.isPresent()) {
+			userRepository.deleteById(id);
+			return ResponseEntity.ok().build();
 		}
 		
 		return ResponseEntity.notFound().build();
--- src/main/java/br/com/inatel/icc/pastebean/controller/form/UpdateUserForm.java
+++ src/main/java/br/com/inatel/icc/pastebean/controller/form/UpdateUserForm.java
@@ -1,9 +1,5 @@
 package br.com.inatel.icc.pastebean.controller.form;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import br.com.inatel.icc.pastebean.model.User;
 import br.com.inatel.icc.pastebean.repository.UserApi;
 import br.com.inatel.icc.pastebean.repository.UserRepository;
