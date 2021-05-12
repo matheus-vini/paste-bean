f0071c01fbbf99dbbde63b0c118e3e7821c5f57a
Matheus Vinícius Cirino e Silva <matheus.vinicius@inatel.br>
2021-05-12 15:58:02 -0300

    Implementing Delete method on PasteController

--- src/main/java/br/com/inatel/icc/pastebean/controller/PasteController.java
+++ src/main/java/br/com/inatel/icc/pastebean/controller/PasteController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,7 @@ public class PasteController {
 	private PasteRepository pasteRepository;
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<PasteDto> createPaste(@RequestBody @Valid PasteFormDto pasteForm, UriComponentsBuilder uriBuilder) {
 		Paste paste = pasteForm.convert();
 		pasteRepository.save(paste);
@@ -58,5 +60,12 @@ public class PasteController {
 		Paste paste = pasteForm.update(id, pasteRepository);
 		
 		return ResponseEntity.ok(new PasteDto(paste));
+	}
+	
+	@DeleteMapping("{id}")
+	@Transactional
+	public ResponseEntity<?> deletePastes(@PathVariable Long id) {
+		pasteRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
