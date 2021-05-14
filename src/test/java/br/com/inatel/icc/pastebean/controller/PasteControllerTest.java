2e1ead13f06732f1d258c4aadaa8ef4e604499d7
Matheus Vinícius Cirino e Silva <matheus.vinicius@inatel.br>
2021-05-14 10:26:38 -0300

    Implementing Paste tests + bugfix on User tests

--- src/test/java/br/com/inatel/icc/pastebean/controller/PasteControllerTest.java
+++ src/test/java/br/com/inatel/icc/pastebean/controller/PasteControllerTest.java
@@ -2,9 +2,9 @@ package br.com.inatel.icc.pastebean.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
 
+import org.junit.Test;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,61 +24,76 @@ import br.com.inatel.icc.pastebean.config.db.Query;
 @AutoConfigureMockMvc
 @ActiveProfiles("tests")
 @TestMethodOrder(OrderAnnotation.class)
-class UserControllerTest {
-
+public class PasteControllerTest {
+	
 	@Autowired
 	private MockMvc mockMvc;
 	
-	String uri = "/users";
+	String uri = "/pastes";
 	
 	@Test
-	public void shouldShowUsersNameAndId() throws Exception {
+	public void shouldShowPastesIdTitleAndContent() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders
 						.get(uri))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().string(containsString("id")))
-				.andExpect(MockMvcResultMatchers.content().string(containsString("username")));
+				.andExpect(MockMvcResultMatchers.content().string(containsString("title")))
+				.andExpect(MockMvcResultMatchers.content().string(containsString("content")));
+	}
+	
+	@Test
+	public void shouldShowPasteOfId2() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders
+						.get(uri+"/2"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("id")))
+				.andExpect(MockMvcResultMatchers.content().string(containsString("title")))
+				.andExpect(MockMvcResultMatchers.content().string(containsString("content")))
+				.andExpect(MockMvcResultMatchers.content().string(containsString("Bob")));
 	}
 	
 	@Test
 	@Order(1)
-	public void shouldCreateUser() throws Exception {
-		String json = "{\"username\":\"Katamari\","
-					 + "\"password\":\"Damacy\"}";
+	public void shouldCreatePaste() throws Exception {
+		String json = "{\"title\":\"Drag on a Sphere\","
+					 + "\"content\":\"The aerodynamic drag on an object depends on several factors, "
+					 + "including the shape, size, inclination, and flow conditions. "
+					 + "A sphere has symmetry around all three axes, and therefore its angle of attack never varies.\","
+					 + "\"privacy\":\"PUBLIC\"}";
 		
 		mockMvc.perform(MockMvcRequestBuilders
 						.post(uri)
 						.content(json)
 						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isCreated())
-				.andExpect(MockMvcResultMatchers.content().string(containsString("Katamari")));
+				.andExpect(MockMvcResultMatchers.content().string(containsString("sphere")));
 	}
 	
 	@Test
 	@Order(2)
-	public void shouldChangeUsernameAndPasswordOfNewlyCreatedUser() throws Exception {
-		String json = "{\"username\":\"Megaman\","
-				 + "\"password\":\"Protoman\"}";
+	public void shouldChangeTitleAndContentOfNewlyCreatedPaste() throws Exception {
+		String json = "{\"title\":\"Drag on a Banana\","
+				 + "\"content\":\"There are no conclusive studies on air drag on a banana.\"}";
 		
-		String id = Query.highestUserId().toString();
+		String id = Query.highestPasteId().toString();
 		
 		mockMvc.perform(MockMvcRequestBuilders
 						.put(uri+"/"+id)
 						.content(json)
 						.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string(containsString("Megaman")))
+				.andExpect(MockMvcResultMatchers.content().string(containsString("banana")))
 				.andExpect(MockMvcResultMatchers.content().string(containsString(id)));
 	}
 	
 	@Test
 	@Order(3)
 	public void shouldDeleteNewlyCreatedUser() throws Exception {
-		Long id = Query.highestUserId();
+		Long id = Query.highestPasteId();
 		
 		mockMvc.perform(MockMvcRequestBuilders
 						.delete(uri+"/"+id))
 		.andExpect(MockMvcResultMatchers.status().isOk());
 	}
-
+	
 }
